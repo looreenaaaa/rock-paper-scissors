9678a123c65f0a002e6f0a11b6cc3a0c00e92549
loren <lorenappff@gmail.com>
2025-08-17 22:22:06 +0200

    function getComputerChoice() correctly attempted

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    const randomNum = Math.random();
+
+    if (randomNum < 1/3) {
+        return "rock";
+    } else if (randomNum < 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
